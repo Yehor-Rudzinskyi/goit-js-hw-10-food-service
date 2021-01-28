06828df3baa6abc9f9d06fa6e330b0bcc6974d37
Yehor <65769717+Yehor-Rudzinskyi@users.noreply.github.com>
2021-01-28 22:08:57 +0200

    newOne

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,24 +3,73 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const { LIGHT, DARK } = Theme;
+
 const themeRef = document.querySelector('.theme-switch__toggle');
 const bodyRef = document.querySelector('body');
 
-if (localStorage.getItem('settings')) {
-    let settings = localStorage.getItem('settings');
-    bodyRef.classList.add(settings)
-    if (settings === 'dark-theme') {
-        themeRef.checked = true;
-    }
-};
+let storageIn = localStorage.getItem('theme');
 
-themeRef.addEventListener('change', function () {
-    bodyRef.className = '';
-    if (this.checked) {
-        bodyRef.classList.add(Theme.DARK);
-        localStorage.setItem('settings', Theme.DARK)
-    } else {
-        bodyRef.classList.add(Theme.LIGHT);
-        localStorage.setItem('settings', Theme.LIGHT)
+switch (storageIn) {
+    case null:
+         bodyRef.classList.add('light-theme')
+        localStorage.setItem('theme', `${LIGHT}`)
+        break;
+    case 'light-theme':
+              bodyRef.classList.add('light-theme')
+
+        break;
+        case 'dark-theme':
+            bodyRef.classList.add('dark-theme')
+            themeRef.setAttribute('checked', true) 
+            break;
+            }
+            
+themeRef.addEventListener('change', event => {
+    const cheker = event.target.checked;
+    switch (cheker) {
+        case true:
+             themeRef.setAttribute('checked', true) 
+            bodyRef.classList.replace('light-theme', 'dark-theme')
+            localStorage.setItem('theme', `${DARK}`)
+                break;
+        case false:
+             themeRef.setAttribute('checked', false)
+            bodyRef.classList.replace('dark-theme','light-theme')
+            localStorage.setItem('theme', `${LIGHT}`)
+            break;
     }
 });
+
+//     if (cheker) {
+//          themeRef.setAttribute('checked', true) 
+//     bodyRef.classList.add('dark-theme')
+//     localStorage.setItem('theme', `${DARK}`)
+//     } else {
+//     themeRef.setAttribute('checked', false)
+//     bodyRef.classList.add('light-theme')
+//     localStorage.setItem('theme', `${LIGHT}`)
+// }
+
+
+
+
+
+// if (localStorage.getItem('settings')) {
+//     let settings = localStorage.getItem('settings');
+//     bodyRef.classList.add(settings)
+//     if (settings === 'dark-theme') {
+//         themeRef.checked = true;
+//     }
+// };
+
+// themeRef.addEventListener('change', function () {
+//     bodyRef.className = '';
+//     if (this.checked) {
+//         bodyRef.classList.add(Theme.DARK);
+//         localStorage.setItem('settings', Theme.DARK)
+//     } else {
+//         bodyRef.classList.add(Theme.LIGHT);
+//         localStorage.setItem('settings', Theme.LIGHT)
+//     }
+// });
